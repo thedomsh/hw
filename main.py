ad935c659c3beff9dd75e81a07fbeab069c4c308
thedomsh <viktor2004tockat@gmail.com>
2022-06-24 14:01:03 +0500

    initial

---
+++ main.py
@@ -0,0 +1,62 @@
+# 1
+
+# list_1 = [2, 134, 3243, 32, 2434, 2234]
+# max_num = max(list_1)
+# print(max_num)
+
+# 2
+
+# list_1 = (8, 2, 3, 0, 7)
+# sum = 0
+# for i in list_1:
+#     sum += i
+# print(sum)
+
+# 3
+
+# list_1 = (8, 2, 3, -1, 7)
+# multiply = 1
+# for i in list_1:
+#     multiply *= i
+# print(multiply)
+
+# 4
+
+# print("1234abcd"[::-1])
+
+# 5
+
+# num = int(input())
+# f = 1
+# for i in range(2, num + 1):
+#     f *= i
+# print(f)
+
+# 6
+
+# str_1 = "The quick Brow Fox"
+# print(f"Заглавных букв: {sum(map(str.isupper, str_1))}\n"
+#       f"Строчных букв: {sum(map(str.islower, str_1))}")
+
+# 7
+
+# word = str(input("Введите слово: ")).lower()
+# word_1 = word[::-1]
+# if word == word_1:
+#     print(True)
+# else:
+#     print(False)
+
+# 8
+
+# x = int(input())
+# y = int(input())
+# s = (1.1 ** y) * x
+# print(s)
+
+# 9
+
+# list_1 = [45, 55, 60, 37, 100, 105, 220]
+# for i in list_1:
+#     if i % 15 == 0:
+#         print(i)
